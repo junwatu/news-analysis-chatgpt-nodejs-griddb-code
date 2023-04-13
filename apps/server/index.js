04318a91b6d816252117707bfbc1844e48645451
Equan P <equan.p@gmail.com>
2023-04-14 04:42:34 +0700

    enable cors and add gentags route

--- apps/server/index.js
+++ apps/server/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import { fetchMultiNews } from './fetchMultiNews.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const corsOptions = {
+	origin: 'http://localhost:3001',
+};
 
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get('/multinews', async (req, res) => {
@@ -14,6 +19,10 @@ app.get('/multinews', async (req, res) => {
 		res.status(500).json({ error: 'Error fetching multi news' });
 	}
 });
+
+app.get('/gentags', async (req, res) => {
+	res.json({ action: "not implmented yet!" })
+})
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
