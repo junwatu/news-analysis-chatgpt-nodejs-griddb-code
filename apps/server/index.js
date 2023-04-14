10080b7a632eb9516d37f71bb8f7b5274b79dea1
Equan P <equan.p@gmail.com>
2023-04-14 10:58:39 +0700

    return data as JSON and init GridDB

--- apps/server/index.js
+++ apps/server/index.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import { fetchMultiNews } from './fetchMultiNews.js';
+import { generateTagsFromNews } from './genTags.js';
+import * as GridDB from "./griddb.cjs"
+const { collectionDb, store, conInfo } = await GridDB.initGridDbTS();
+
+console.log(await GridDB.containersInfo(store))
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,14 +19,23 @@ app.use(express.json());
 app.get('/multinews', async (req, res) => {
 	try {
 		const newsData = await fetchMultiNews();
-		res.status(200).json(newsData);
+		const newsDataObject = {}
+		newsData.forEach((element, index) => {
+			const news = element?.row?.document;
+			newsDataObject[index + 1] = news;
+		});
+		res.status(200).json(newsDataObject);
 	} catch (error) {
 		res.status(500).json({ error: 'Error fetching multi news' });
 	}
 });
 
 app.get('/gentags', async (req, res) => {
-	res.json({ action: "not implmented yet!" })
+	const newsData = await fetchMultiNews();
+	const sampleNews = newsData[3].row.document;
+	const tags = await generateTagsFromNews(sampleNews)
+
+	res.json({ news: sampleNews, tags: tags.content })
 })
 
 app.listen(port, () => {
